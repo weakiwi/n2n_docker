e926c607c51a2e2a86bdccaa8efcca26d909845e
weakiwi <670932228@qq.com>
2018-06-06 18:18:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ edgepipe=/tmp/f$RANDOM
 dhcppipe=/tmp/d$RANDOM
 mkfifo $edgepipe
 mkfifo $dhcppipe
-if [ -z $SUPERNODE ]
+if [ -n $SUPERNODE ]
 then
-    supernode -l 5353 -f
+    supernode -l $N2N_SUPERNODE_PORT -f
 fi
-if [ -z $STATIC_IP ]
+if [ -n $STATIC_IP ]
 then
     nohup edge -d $N2N_INTERFACE -a dhcp:0.0.0.0 -c $N2N_GROUP -k $N2N_PASS -l $N2N_SERVER -f -r > /workdir/n2n.log &
     n2n_interface_ip=`/sbin/ifconfig $N2N_INTERFACE|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
